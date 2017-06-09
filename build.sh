f0b5501b688b034efb51807a28d7da96cc052ffb
Sebastian Wittka <sebastian.wittka@gmail.com>
2017-06-09 14:16:00 +0200

    split commands in script into multiple ones

--- build.sh
+++ build.sh
@@ -1,1 +1,7 @@
-pdflatex -interaction=nonstopmode master_thesis_wittka.tex | bibtex master_thesis_wittka.aux | makeindex master_thesis_wittka.nlo -s /home/swittka/texmf/tex/latex/common/nomencl.ist -o master_thesis_wittka.nls | pdflatex -interaction=nonstopmode master_thesis_wittka.tex | pdflatex -interaction=nonstopmode master_thesis_wittka.tex
+#pdflatex -interaction=nonstopmode master_thesis_wittka.tex | bibtex master_thesis_wittka.aux | makeindex master_thesis_wittka.nlo -s /home/swittka/texmf/tex/latex/common/nomencl.ist -o master_thesis_wittka.nls | pdflatex -interaction=nonstopmode master_thesis_wittka.tex | pdflatex -interaction=nonstopmode master_thesis_wittka.tex
+
+pdflatex -interaction=nonstopmode master_thesis_wittka.tex
+bibtex master_thesis_wittka.aux
+makeindex master_thesis_wittka.nlo -s /home/swittka/texmf/tex/latex/common/nomencl.ist -o master_thesis_wittka.nls
+pdflatex -interaction=nonstopmode master_thesis_wittka.tex
+pdflatex -interaction=nonstopmode master_thesis_wittka.tex
